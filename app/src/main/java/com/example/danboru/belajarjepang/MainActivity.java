a7c15f36e09d97de8edcebfb1c4c3a8976e794a7
Danang <8danang8@gmail.com>
2016-05-24 16:55:40 +0700

    Penambahan gambar

--- app/src/main/java/com/example/danboru/belajarjepang/MainActivity.java
+++ app/src/main/java/com/example/danboru/belajarjepang/MainActivity.java
@@ -1,14 +1,15 @@
 package com.example.danboru.belajarjepang;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     ListView listview;
     private String[] data_menu;
@@ -17,6 +18,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+
         setContentView(R.layout.activity_main);
 
         listview = (ListView) findViewById(R.id.list_menu);
